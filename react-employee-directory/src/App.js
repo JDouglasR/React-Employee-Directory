eb4d134d1e5916a7adc23da3942f555afe294a45
Justin Roose <justin.d.roose@gmail.com>
2020-10-09 23:36:03 -0400

    app js done "

--- react-employee-directory/src/App.js
+++ react-employee-directory/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Main from "./components/Main/Main";
+import MemberWrapper from "./components/MemberWrapper/MemberWrapper";
+import MemberCard from "./components/MemberCard/MemberCard";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    loading: true,
+    people: [],
+  };
+
+  async componentDidMount() {
+    const url = "https://randomuser.me/api/?results=200&nat=us";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ loading: false, people: data.results });
+  }
+
+  render() {
+    const employees = this.state.people;
+    return (
+      <React.Fragment>
+        <Main>
+          {this.state.loading || !this.state.people ? (
+            <div>loading...</div>
+          ) : (
+            <div>results...</div>
+          )}
+          <MemberWrapper>
+            {employees.map((person) => (
+              <MemberCard
+                key={person.id.value}
+                name={person.name.first + " " + person.name.last}
+                img={person.picture.large}
+                location={person.location.city}
+                phone={person.phone}
+                email={person.email}
+              />
+            ))}
+          </MemberWrapper>
+        </Main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
